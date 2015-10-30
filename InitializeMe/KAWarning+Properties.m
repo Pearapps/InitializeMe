6a7f1d8076b592474c7e3f6b7a5a3f36a90d1e6a
Kenny Ackerson <kenny@tumblr.com>
2015-10-29 23:47:36 -0400

    Warning starting

---
+++ InitializeMe/KAWarning+Properties.m
@@ -0,0 +1,20 @@
+//
+//  KAWarning+Properties.m
+//  InitializeMe
+//
+//  Created by Kenny Ackerson on 10/29/15.
+//  Copyright © 2015 Kenneth Parker Ackerson. All rights reserved.
+//
+
+#import "KAWarning+Properties.h"
+
+@implementation KAWarning (Properties)
+
++ (KAWarning *)warningForProperty:(Property *)property {
+    
+    
+    
+    return nil;
+}
+
+@end
---
+++ InitializeMe/KAWarning.h
@@ -0,0 +1,24 @@
+//
+//  KAWarning.h
+//  InitializeMe
+//
+//  Created by Kenny Ackerson on 10/29/15.
+//  Copyright © 2015 Kenneth Parker Ackerson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSUInteger, WarningLevel) {
+    WarningLevelLow,
+    WarningLevelMedium,
+    WarningLevelHigh
+};
+
+@interface KAWarning : NSObject
+
+@property (nonatomic, readonly, nonnull) NSString *reason;
+@property (nonatomic, readonly) WarningLevel warningLevel;
+
+- (nonnull instancetype)initWithReason:(nonnull NSString *)reason warningLevel:(WarningLevel)warningLevel;
+
+@end
