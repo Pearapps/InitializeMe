36b38e8b410e1571323a35ed73d30045005eb3f6
kenny <pearapps@gmail.com>
2016-01-23 15:56:55 -0500

    Swift support working i think

---
+++ InitializeMe/KASwiftInitializerWriter.m
@@ -0,0 +1,43 @@
+//
+//  KASwiftInitializerWriter.m
+//  InitializeMe
+//
+//  Created by Kenneth Parker Ackerson on 1/23/16.
+//  Copyright © 2016 Kenneth Parker Ackerson. All rights reserved.
+//
+
+#import "KASwiftInitializerWriter.h"
+
+@interface KASwiftInitializerWriter ()
+
+@property (nonatomic, readonly) NSArray <Property *> *properties;
+
+@end
+
+@implementation KASwiftInitializerWriter
+
+- (instancetype)initWithProperties:(NSArray <Property *> *)properties {
+    self = [super init];
+    if (self) {
+        _properties = properties;
+    }
+    return self;
+}
+
+- (NSString *)initializer {
+    NSString *inititalizer = @"init(";
+    
+    for (Property *property in self.properties) {
+        inititalizer = [inititalizer stringByAppendingFormat:@"%@: %@", property.variable, property.type];
+    }
+    
+    inititalizer = [inititalizer stringByAppendingString:@") {\n"];
+
+    for (Property *property in self.properties) {
+        inititalizer = [inititalizer stringByAppendingFormat:@"\tself.%@ = %@\n", property.variable, property.variable];
+    }
+    
+    return [inititalizer stringByAppendingString:@"}"];
+}
+
+@end
