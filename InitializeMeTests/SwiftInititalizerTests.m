86551e04792a69683a24c7190b73c698583feed9
kenny <pearapps@gmail.com>
2016-01-30 13:29:20 -0500

    Adding tests

--- InitializeMeTests/SwiftInititalizerTests.m
+++ InitializeMeTests/SwiftInititalizerTests.m
@@ -32,6 +32,71 @@
     XCTAssert([output isEqualToString:expectedOutput]);
 }
 
+- (void)testOneFunctionProperty {
+    NSString *expectedOutput = @"init(function: () -> ()) {\n"
+    "\tself.function = function\n"
+    "}";
+    
+    NSString *input = @"let function: () -> ()";
+    
+    id properties = [[[PropertyParser alloc] initWithString:input] properties];
+    
+    [properties makeObjectsPerformSelector:@selector(parse)];
+    
+    NSString *output = [[KAInitializerWriterFactory initializerWriterForProperties:properties] initializer];
+    
+    XCTAssert([output isEqualToString:expectedOutput]);
+}
+
+- (void)testTwoFunctionProperty {
+    NSString *expectedOutput = @"init(function: () -> (), otherFunction: String -> Array<Int>) {\n"
+    "\tself.function = function\n"
+    "\tself.otherFunction = otherFunction\n"
+    "}";
+    
+    NSString *input = @"let function: () -> ()\nlet otherFunction: String -> Array<Int>";
+    
+    id properties = [[[PropertyParser alloc] initWithString:input] properties];
+    
+    [properties makeObjectsPerformSelector:@selector(parse)];
+    
+    NSString *output = [[KAInitializerWriterFactory initializerWriterForProperties:properties] initializer];
+    
+    XCTAssert([output isEqualToString:expectedOutput]);
+}
+
+- (void)testOnePropertyGenericProperty {
+    NSString *expectedOutput = @"init(array: Array<String>) {\n"
+    "\tself.array = array\n"
+    "}";
+    
+    NSString *input = @"let array: Array<String>";
+    
+    id properties = [[[PropertyParser alloc] initWithString:input] properties];
+    
+    [properties makeObjectsPerformSelector:@selector(parse)];
+    
+    NSString *output = [[KAInitializerWriterFactory initializerWriterForProperties:properties] initializer];
+    
+    XCTAssert([output isEqualToString:expectedOutput]);
+}
+
+- (void)testSpecialDictionarySyntaxOneProperty {
+    NSString *expectedOutput = @"init(array: [String]) {\n"
+    "\tself.array = array\n"
+    "}";
+    
+    NSString *input = @"let array: [String]";
+    
+    id properties = [[[PropertyParser alloc] initWithString:input] properties];
+    
+    [properties makeObjectsPerformSelector:@selector(parse)];
+    
+    NSString *output = [[KAInitializerWriterFactory initializerWriterForProperties:properties] initializer];
+    
+    XCTAssert([output isEqualToString:expectedOutput]);
+}
+
 - (void)testTwoProperties {
     NSString *expectedOutput = @"init(window: NSWindow, ello: NSWindow) {\n"
     "\tself.window = window\n"
