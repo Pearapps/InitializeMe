9513f5a41cf4e6e91e679364dcc69ca0704013c9
kenny <pearapps@gmail.com>
2016-01-30 12:36:20 -0500

    Adding property things

--- InitializeMePlugin/InitializeMePlugin.m
+++ InitializeMePlugin/InitializeMePlugin.m
@@ -7,10 +7,14 @@
 //
 
 #import "InitializeMePlugin.h"
+#import <objc/objc.h>
 
 @interface InitializeMePlugin()
 
 @property (nonatomic, strong, readwrite) NSBundle *bundle;
+
+@property (nonatomic) NSString *currentlySelectedText;
+
 @end
 
 @implementation InitializeMePlugin
@@ -21,14 +25,28 @@
 
 - (id)initWithBundle:(NSBundle *)plugin {
     if (self = [super init]) {
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleSelection:) name:@"DVTSourceExpressionSelectedExpressionDidChangeNotification" object:nil];
+        
         // reference to plugin's bundle, for resource access
         self.bundle = plugin;
         [[NSNotificationCenter defaultCenter] addObserver:self
                                                  selector:@selector(didApplicationFinishLaunchingNotification:)
                                                      name:NSApplicationDidFinishLaunchingNotification
                                                    object:nil];
+        
     }
     return self;
+}
+
+- (void)handleSelection:(NSNotification *)notification {
+    @try {
+        NSTextView *textView = [notification.object textView];
+        self.currentlySelectedText = [[textView string] substringWithRange:[textView selectedRange]];
+    }
+    @catch (NSException *exception) {
+        
+    }
 }
 
 - (void)didApplicationFinishLaunchingNotification:(NSNotification*)noti {
@@ -40,14 +58,13 @@
     NSMenuItem *menuItem = [[NSApp mainMenu] itemWithTitle:@"Edit"];
     if (menuItem) {
         [[menuItem submenu] addItem:[NSMenuItem separatorItem]];
-        NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Do Action" action:@selector(doMenuAction) keyEquivalent:@""];
-        //[actionMenuItem setKeyEquivalentModifierMask:NSAlphaShiftKeyMask | NSControlKeyMask];
+        NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Make Initializer And Copy" action:@selector(doMenuAction) keyEquivalent:@""];
+        //[actionMenuItem setKeyEquivalentModifierMask:NSShiftKeyMask | NSControlKeyMask];
         [actionMenuItem setTarget:self];
         [[menuItem submenu] addItem:actionMenuItem];
     }
 }
 
-// Sample Action, for menu item:
 - (void)doMenuAction {
     NSAlert *alert = [[NSAlert alloc] init];
     [alert setMessageText:@"Hello, World"];
